209577d021294070624c0324e5aedd9a9191fc21
Ahmad Shidqi Firdaus <32896532+ahmadkikok@users.noreply.github.com>
2021-04-05 13:33:55 +0700

    Follow up

--- app/Http/Controllers/v1/Owner/SecurityScheduleController.php
+++ app/Http/Controllers/v1/Owner/SecurityScheduleController.php
@@ -20,7 +20,7 @@ class SecurityScheduleController extends Controller
 	{
         try 
         {
-            $security_schedule = $id ? SecuritySchedule::with('security_plan.people')->where('id_site_schedule', $id) : SecuritySchedule::get();
+            $security_schedule = $id ? SecuritySchedule::with('security_plan.people')->where('id_site_schedule', $id)->get() : SecuritySchedule::get();
 
             return $this->respHandler->success('Success get data.', $security_schedule);
         } 
